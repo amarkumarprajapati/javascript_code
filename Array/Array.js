750ed7d6ef1ba7649944835dbcbd2559e21e5ff7
Amar Kumar Prajapati <100253987+amarkumarprajapati@users.noreply.github.com>
2025-01-27 18:39:43 +0530

    added files

---
+++ Array/Array.js
@@ -0,0 +1,48 @@
+let num = [1, 2, 3, 4];
+
+// Map - Reduce - Filter  -
+
+/////////////////////////// map -
+
+// let newarr =  num.map((item,index,array)=>{
+//     console.log(item + index)
+// })
+
+// console.log(newarr)
+
+/////////////////////////// filter -
+
+// num.filter((a) => {
+//   console.log(a < 10);
+//   return a < 100;
+// });
+
+// output -
+
+// true
+// false
+// false
+// false
+// false
+// true
+// false
+// false
+// false
+// false
+// true
+// false
+// false
+// true
+// false
+// false
+
+///////////////////////// Reduce -
+
+// let data =  num.reduce((a,b)=>{
+//     return a + b
+// })
+
+// console.log(data)
+
+// output -
+// 10
---
+++ Factorial.js
@@ -0,0 +1,8 @@
+// Factorial -
+// let a = n*(n-1)*(n-2)*(n-3)*(n-4)*(n-5)
+
+let a = 54;
+
+for (let i of a) {
+  console.log(i);
+}
