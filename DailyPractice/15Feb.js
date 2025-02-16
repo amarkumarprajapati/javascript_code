675c8937511136f01409b1fdcad56e619c017d95
Amar Kumar Prajapati <100253987+amarkumarprajapati@users.noreply.github.com>
2025-02-16 12:53:45 +0530

    dailypractice

---
+++ DailyPractice/15Feb.js
@@ -0,0 +1,10 @@
+// 1. Find the Largest Number in an Array
+// Write a function to find the largest number in an array.
+
+
+
+let data  = [1,69,1,66,999]
+
+function largestnum(arrdata){
+    let largestdata = arrdata.length()
+}
