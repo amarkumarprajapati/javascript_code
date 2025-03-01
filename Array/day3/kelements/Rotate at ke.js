38a852ae91c905963f533edf87b23fc0407c5f6a
amarkumarworkcodesis <167982410+amarkumarworkcodesis@users.noreply.github.com>
2025-03-01 08:13:13 +0530

    added prime number and k term

---
+++ Array/day3/kelements/Rotate at ke.js
@@ -0,0 +1,16 @@
+let data = [1, 21, 321315, 21321, 5, 54686845, 34684];
+let k = 3;
+
+function kterm(data, k) {
+  let newkterm = [];
+  for (let a = data.length - 1; a >= k - 1; a--) {
+    newkterm.push(data[a]);
+  }
+  for (let a = 0; a < k; a++) {
+    newkterm.push(data[a]);
+  }
+  return newkterm;
+}
+
+let res = kterm(data, k);
+console.log(res);
--- Array/day3/primenumber.js
+++ Array/day3/primenumber.js
@@ -2,13 +2,6 @@ let data = [
   135, 151516, 46, 8, 41534, 6854, 6484, 531231, 31, 35, 484, 684, 435, 5,
 ];
 
-function isprimenumber(numberdata) {
-  if (numberdata <= 1) return false;
-  for (let a = 2; a <= Math.sqrt(numberdata); a++) {
-    if (numberdata % a === 0) return false;
-  }
-  return true;
-}
 
 function primenumber(data) {
   let primenumbr = [];
@@ -19,6 +12,14 @@ function primenumber(data) {
     }
   }
   return primenumbr;
+}
+
+function isprimenumber(numberdata) {
+  if (numberdata <= 1) return false;
+  for (let a = 2; a <= Math.sqrt(numberdata); a++) {
+    if (numberdata % a === 0) return false;
+  }
+  return true;
 }
 
 let res = primenumber(data);
