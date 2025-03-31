377e6f6580668a6e754852e0c8ee2f7e128f99fe
amarkumarworkcodesis <167982410+amarkumarworkcodesis@users.noreply.github.com>
2025-03-31 17:24:36 +0530

    added convert an number to string

---
+++ easyprob/arrInclude.js
@@ -0,0 +1,6 @@
+let data = [3,5,3,51,15,3,1,51,5,1,32,15,15,4]
+
+
+let res = data.includes(1)
+
+console.log(res)
---
+++ easyprob/arrindex.js
@@ -0,0 +1,5 @@
+let data = [1, 15, 4, 315, 431, 45, 65, 484, 848]
+
+let value = data.toString().split(",")
+let res = value.indexOf('431')
+console.log(res)
