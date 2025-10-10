b07139aed3ad3e5af28bb9d7edac8b2e05355b22
Amar Kumar Prajapati <100253987+amarkumarprajapati@users.noreply.github.com>
2025-10-10 12:00:01 +0530

    Updated Folder

--- Array/Interview/Interview3.js
+++ Array/Interview/Interview3.js
@@ -4,7 +4,7 @@ function extractElements(nestedArray) {
     let result = [];
     for (let element of nestedArray) {
         if (Array.isArray(element)) {
-          
+
         } else {
             result.push(element);
         }
